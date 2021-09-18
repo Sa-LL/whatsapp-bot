e4e93479d6c59979bed2aa968ee29aeb684114ac
Santiago <santiago.londono@utp.edu.co>
2021-09-17 21:31:07 -0500

    sends messages

--- index.js
+++ index.js
@@ -5,21 +5,23 @@ async function main() {
         // Configures puppeteer
         const browser = await puppeteer.launch({ headless: false });
         const page = await browser.newPage();
-        // await page.setUserAgent(
-        //     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
-        // );
+        await page.setUserAgent(
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+        );
 
         // Navigates to Whatsapp
         await page.goto("https://web.whatsapp.com/");
         await page.waitForSelector(".ldL67");
         await delay(3000);
         const contactName = "Dona 😈";
-        await page.click(`span[title="${contactName}"]`);
-        await page.waitForSelector("_2lMWa");
 
+        await page.click(`span[title="${contactName}"]`);
+        // await page.waitForSelector("_2lMWa");
+        await delay(500);
+        console.log("message");
         // Finds the message bar and focuses on it
-        const editor = await page.$('div[div-tab="9"]');
-        await editor.focus();
+        // const editor = await page.$('div[div-tab="9"]');
+        // await editor.focus();
 
         const amountOfMessages = 10;
 
